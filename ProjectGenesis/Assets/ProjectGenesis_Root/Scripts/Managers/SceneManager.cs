2bff49f045662c8cd2fdb9752d80cea51f6f5b16
pablofon <153851706+pablofon@users.noreply.github.com>
2024-05-13 16:20:32 +0200

    dfghjk

---
+++ ProjectGenesis/Assets/ProjectGenesis_Root/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+    public void SceneLoader(int sceneToLoad)
+    {
+        //SceneManager.LoadScene(sceneToLoad); Preguntar a Jorge
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("saliendo de Juego");
+        Application.Quit();
+    }
+
+
+}
