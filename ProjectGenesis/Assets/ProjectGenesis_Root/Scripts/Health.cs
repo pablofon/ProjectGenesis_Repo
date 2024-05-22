a8b9dabfc35a8701e6b235c9cccb86557ac746e6
YisusCrist88 <153851618+YisusCrist88@users.noreply.github.com>
2024-05-22 15:28:37 +0200

    dfghjkl

--- ProjectGenesis/Assets/ProjectGenesis_Root/Scripts/Health.cs
+++ ProjectGenesis/Assets/ProjectGenesis_Root/Scripts/Health.cs
@@ -2,14 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+[System.Serializable]
+public class MyUnityEvent : UnityEvent { }
 
 public class Health : MonoBehaviour
 {
-    private float health;
+    [SerializeField] private float health;
 
+    public MyUnityEvent damage;
+    public MyUnityEvent death;
 
-
-    public void Damage(float dmg) { health -= dmg; }
+    public void Damage(float dmg)
+    {
+        if (health - dmg > 0)
+        {
+            health -= dmg;
+            damage.Invoke();
+        }
+        else death.Invoke();
+    }
 
     public void Heal(float healing) { health += healing; }
 }
