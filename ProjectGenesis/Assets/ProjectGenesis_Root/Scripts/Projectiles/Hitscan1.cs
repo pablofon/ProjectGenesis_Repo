0cbad5652e9f9ca0257cc2d620bbd402b32841a4
YisusCrist88 <153851618+YisusCrist88@users.noreply.github.com>
2024-05-22 15:40:17 +0200

    dfghjkl

--- ProjectGenesis/Assets/ProjectGenesis_Root/Scripts/Projectiles/Hitscan1.cs
+++ ProjectGenesis/Assets/ProjectGenesis_Root/Scripts/Projectiles/Hitscan1.cs
@@ -20,7 +20,7 @@ public class Hitscan1 : MonoBehaviour
         for (int i = 0; hits.Length > i && pierce > i && hits[i].collider.contactCaptureLayers != groundLayer; i++)
         {
             Health healthScript = hits[i].collider.GetComponent<Health>();
-            if (healthScript != null) healthScript.Damage(-dmg);
+            if (healthScript != null) healthScript.Damage(dmg);
             endPos = hits[i].point;
             distance = hits[i].distance;
         }
